2c794c456778671539074623f1cf5088f1342a1f
taco <tacowu@outlook.com>
2023-06-18 13:54:39 +0800

    code fix

--- myML_backup/cnn_graph_view.py
+++ myML_backup/cnn_graph_view.py
@@ -1,11 +1,11 @@
 import os, sys
-import tensorflow as tf
-from tensorflow import keras
+#import tensorflow as tf
+#from tensorflow import keras
 import tkinter as tk
 
 
 class MyDraw(tk.Frame):
-    def __init__(self, master, height=600, width=1000):
+    def __init__(self, master, height=600, width=800):
         super(MyDraw, self).__init__(master)
         self.width = width
         self.height = height
@@ -15,19 +15,22 @@ class MyDraw(tk.Frame):
         self.pack()
 
     
-    def draw_input_node(self, x0, y0, dr, input_text, 
+    def draw_node(self, x0, y0, dr, _text, 
         out_len, out_width, out_dir):
         """
         """
         self.canvas.create_oval(x0-dr, y0-dr, x0+dr, y0+dr, fill='black')
-        self.canvas.create_text(x0, y0-10, text=input_text)
+        #self.canvas.create_text(x0, y0-10, text=input_text)
         x1, y1 = (0,0)
         if out_dir=='e':
             x1, y1 = x0+dr+out_len, y0
+            self.canvas.create_text(x0-15, y0-15, text=_text)
         elif out_dir=='s':
             x1, y1 = x0, y0+dr+out_len
+            self.canvas.create_text(x0, y0-15, text=_text)
         elif out_dir=='n':
             x1, y1 = x0, y0-dr-out_len
+            self.canvas.create_text(x0, y0+15, text=_text)
         else:
             x1, y1 = x0, y0
         self.canvas.create_line(x0, y0, x1, y1, width=out_width)
@@ -64,74 +67,27 @@ class MyDraw(tk.Frame):
         self.canvas.create_text(xc, yc, text=text)
         self.canvas.create_line(x1, y1, x2, y2, width=out_width)
         return (x2, y2)
-
-        
-    def draw_net(self, x0, y0, 
-        add_skip, add_input, input_text, 
-        rec_sizes, rec_text, out_len, out_width, 
-        flowdir='x'):
-        # draw input node and line
-        dr, llen = (0, 0)
-        if add_input:
-            dr, llen, lw = (3, 30, 1)
-        if flowdir=='x':
-            x0, y0 = (x0+dr, y0)
-            x1, y1 = (x0+dr+llen, y0)
-        else:
-            x0, y0 = (x0, y0+dr)
-            x1, y1 = (x0, y0+dr+llen)        
-        if add_input:
-            self.canvas.create_oval(x0-dr, y0-dr, x0+dr, y0+dr, fill='black')
-            self.canvas.create_text(x0, y0-10, text=input_text)
-            self.canvas.create_line(x0, y0, x1, y1, width=lw)
-        # stack of rec        
-        for i, s in enumerate(rec_sizes):
-            xc = x1+s//2 if flowdir=='x' else x1
-            yc = y1 if flowdir=='x' else y1+s//2
-            print(xc, yc)
-            self.draw_conv2d_block(xc, yc, 
-                size=s, text=rec_text, 
-                width=out_width, llen=out_len, flowdir=flowdir)
-            x1 = xc+s//2+out_len if flowdir=='x' else x1
-            y1 = y1 if flowdir=='x' else yc+s//2+out_len
-        
-        if add_skip:
-            y0 = y0+15 if add_input else y0-10
-            self.canvas.create_arc(x0-60, y0, x0+60, y1-10, 
-                extent=180, start=90, style=tk.ARC)
-        return (x1, y1)
-
     
-    def _input_conv2d_output(self, x0, y0, 
-        line_len, 
-        line_w, 
-        input_text, 
-        kerns, 
-        rec_text,
-        ch_w,
-        draw_skip,
-        skip_to_final=False):
-        #
-        self.canvas.create_oval(x0-3, y0-3, x0+3, y0+3, fill='black')
-        self.canvas.create_text(x0+10, y0-10, text=input_text, anchor='nw')
-        self.canvas.create_line(x0, y0, x0, y0+3+line_len, width=line_w)
-        coords_y = [y0, y0+3+line_len]
-        for i, s in enumerate(kerns):
-            draw_conv2d_block(
-               self.canvas, x0, coords_y[-1], s, s, 
-               text=rec_text, ch_w=ch_w, draw_skip=draw_skip)
-            yf = coords_y[-1] + s + line_len
-            coords_y.append(yf)
-        draw_conv2d_block(
-            self.canvas, x0, coords_y[-1], 20, 20, 
-            text='1x1', ch_w=1, draw_skip=False)
-        yf = coords_y[-1]
-        yf = yf + 40
-        self.canvas.create_oval(x0-3, yf-3, x0+3, yf+3, fill='black')
-        self.canvas.create_text(x0+10, yf+3, text='cnn output', anchor='nw')
-        if skip_to_final:
-            self.canvas.create_arc(x0-70, y0+10, x0+70, yf-10, 
-                extent=180, start=90, dash=(10,4), style=tk.ARC)    
+    def draw_skip_arc(self, x0, y0, x1, y1, dr, ifdash=False, out_dir='s'):
+        if out_dir=='e':
+            ulx, uly = x0, y0-dr
+            lrx, lry = x1, y1+dr
+            st, ext = 180, 180
+        elif out_dir=='s':
+            ulx, uly = x0-dr, y0
+            lrx, lry = x1+dr, y1
+            st, ext = 90, 180
+        elif out_dir=='n':
+            ulx, uly = x1-dr, y1
+            lrx, lry = x0+dr, y0
+            st, ext = 270, 180
+        else:
+            pass
+        if ifdash:
+            self.canvas.create_arc(ulx, uly, lrx, lry, start=st, dash=(4,4), extent=ext, style=tk.ARC)
+        else:
+            self.canvas.create_arc(ulx, uly, lrx, lry, start=st, extent=ext, style=tk.ARC)
+            
 
 
 
@@ -139,136 +95,71 @@ class MyDraw(tk.Frame):
 if __name__=="__main__":
     root = tk.Tk()
     root.title("")
-    mydraw = MyDraw(root)
+    mydraw = MyDraw(root, height=900, width=1200)
+    flowdir = 's'
     
+    # general CNN: sequential resNet
     x0, y0 = 100, 50
-    x1, y1 = mydraw.draw_input_node(x0, y0, dr=3, 
-        input_text="",
-        out_len=30,
-        out_width=1,
-        out_dir='s')
-    for _ in range(6):
-        x1, y1 = mydraw.draw_conv2d_block(x1, y1)
-   
+    x1, y1 = mydraw.draw_node(x0, y0, dr=3, _text="vt0 (input image)",
+        out_len=30, out_width=1, out_dir=flowdir)
+    x2, y2 = mydraw.draw_conv2d_block(x1, y1, out_dir=flowdir)
+    x3, y3 = mydraw.draw_conv2d_block(x2, y2, out_dir=flowdir)
+    x4, y4 = mydraw.draw_conv2d_block(x3, y3, out_dir=flowdir)
+    x5, y5 = mydraw.draw_conv2d_block(x4, y4, out_dir=flowdir)
+    x6, y6 = mydraw.draw_conv2d_block(x5, y5, out_dir=flowdir)
+    x7, y7 = mydraw.draw_conv2d_block(x6, y6, out_dir=flowdir)
+    mydraw.draw_skip_arc(x1, y1-10, x3, y3-10, dr=60, out_dir=flowdir)
+    mydraw.draw_skip_arc(x3, y3-10, x5, y5-10, dr=60, out_dir=flowdir)
+    mydraw.draw_skip_arc(x5, y5-10, x7, y7-10, dr=60, out_dir=flowdir)
+    x8, y8 = mydraw.draw_conv2d_block(x7, y7, out_dir=flowdir, size=20, text='1x1', out_len=30, out_width=1)
+    mydraw.draw_node(x8, y8, dr=3, _text="cnn output", 
+        out_len=0, out_width=1, out_dir='n')
+    #mydraw.draw_skip_arc(x0, y0+15, x8, y8-15, dr=70, ifdash=True, out_dir='n')
 
-    """
-    flowdir = 'y'
+    # resNetBlock
+    mydraw.canvas.create_text(250, 160, text="stack of resNetBlock")
+    x0, y0 = 250, 200
+    x1, y1 = mydraw.draw_node(x0, y0, dr=5, _text="x",
+        out_len=30, out_width=3, out_dir=flowdir)
+    x2, y2 = mydraw.draw_conv2d_block(x1, y1, text="resnet_1", out_dir=flowdir)
+    x3, y3 = mydraw.draw_conv2d_block(x2, y2, text="resnet_2", out_dir=flowdir)
+    x4, y4 = mydraw.draw_conv2d_block(x3, y3, text="resnet_3", out_len=30, out_width=3, out_dir=flowdir)  
+    mydraw.draw_node(x4, y4, dr=5, _text="x+delta", 
+        out_len=0, out_width=1, out_dir='n')
 
-    x0, y0 = 150, 50
-    x1, y1 = mydraw.draw_net(x0, y0, 
-             add_skip=True, add_input=True, input_text="vt0 (input image)",
-             rec_sizes=[50,50, 50], rec_text="Conv2D", 
-             out_len=20, out_width=4, flowdir=flowdir)
-        
-    x2, y2 = mydraw.draw_net(x1, y1, 
-             add_skip=True, add_input=False, input_text="",
-             rec_sizes=[50], rec_text="Conv2D", 
-             out_len=20, out_width=4, flowdir=flowdir)
-                 
-    x3, y3 = mydraw.draw_net(x2, y2,
-             add_skip=True, add_input=False, input_text="", 
-             rec_sizes=[50, 50], rec_text='Conv2D',
-             out_len=20, out_width=4, flowdir=flowdir)
+    # UNet
+    x0, y0 = 500, 50
+    x1, y1 = mydraw.draw_node(x0, y0, dr=3, _text="vt0 (input image)",
+        out_len=30, out_width=1, out_dir=flowdir)
+    x2, y2 = mydraw.draw_conv2d_block(x1, y1, out_dir=flowdir)
+    x3, y3 = mydraw.draw_conv2d_block(x2, y2, out_dir=flowdir)
+    x4, y4 = mydraw.draw_conv2d_block(x3, y3, out_dir=flowdir)
+    x5, y5 = mydraw.draw_conv2d_block(x4, y4, out_dir=flowdir)
+    x6, y6 = mydraw.draw_conv2d_block(x5, y5, out_dir=flowdir)
+    x7, y7 = mydraw.draw_conv2d_block(x6-25, y6+25, out_len=60, out_dir='e')
+    mydraw.draw_skip_arc(x1, y1-10, x3, y3-10, dr=60, out_dir=flowdir)
+    mydraw.draw_skip_arc(x3, y3-10, x5, y5-10, dr=60, out_dir=flowdir)
+    #mydraw.draw_skip_arc(x5-40, y5-10, x7-40, y7-10, dr=60, out_dir=flowdir)
+    mydraw.canvas.create_arc(x5-60, y5-10, x7-20, y7+60, start=90, extent=253, style=tk.ARC)
+    x8, y8 = mydraw.draw_conv2d_block(x7+10, y7+10, out_dir='n', size=20, text='1x1', out_len=50, out_width=1)
+    xf, yf = mydraw.draw_conv2d_block(x8, y8, out_dir='n', size=20, text='1x1', out_len=50, out_width=1)
+    xf, yf = mydraw.draw_conv2d_block(xf, yf, out_dir='n', size=20, text='1x1', out_len=50, out_width=1)
+    xf, yf = mydraw.draw_conv2d_block(xf, yf, out_dir='n', size=20, text='1x1', out_len=50, out_width=1)
+    xf, yf = mydraw.draw_conv2d_block(xf, yf, out_dir='n', size=20, text='1x1', out_len=50, out_width=1)
+    xf, yf = mydraw.draw_conv2d_block(xf, yf, out_dir='n', size=20, text='1x1', out_len=50, out_width=1)
+    mydraw.canvas.create_line(x2, y2-10, x2+98, y2-10, arrow=tk.LAST)
+    mydraw.canvas.create_line(x3, y3-10, x3+98, y3-10, arrow=tk.LAST)
+    mydraw.canvas.create_line(x4, y4-10, x4+98, y4-10, arrow=tk.LAST)
+    mydraw.canvas.create_line(x5, y5-10, x5+98, y5-10, arrow=tk.LAST)
+    mydraw.canvas.create_line(x6, y6-10, x6+98, y6-10, arrow=tk.LAST)
+    mydraw.draw_node(xf, yf, out_dir='s', dr=3, _text="cnn output", out_len=1, out_width=1)
+
+    #mydraw.draw_node(x8, y8, dr=3, _text="cnn output", 
+    #    out_len=0, out_width=1, out_dir='n')
+    #mydraw.draw_skip_arc(x0, y0+15, x8, y8-15, dr=70, ifdash=True, out_dir='n')   
     
-    x4, y4 = mydraw.draw_net(x3-10, y3+10,
-             add_skip=False, add_input=False, input_text="", 
-             rec_sizes=[20], rec_text='1x1',
-             out_len=100, out_width=1, flowdir='x')
-             
-    # add output node
-    mydraw.canvas.create_oval(x4-3, y4-3, x4+3, y4+3, fill='black')
-    mydraw.canvas.create_text(x4, y4+10, text="cnn output image")
-    #mydraw.canvas.create_arc(x0-85, y0+10, x0+85, y4-10, 
-    #    extent=180, start=90, dash=(10,4), style=tk.ARC)
-        
-
-    
-    #x0, y0 = 100, 50
-    #mydraw._input_conv2d_output(x0, y0, 
-    #    line_len=20,
-    #    line_w=1,
-    #    input_text="vt0 (input images)",
-    #    kerns=[40, 50, 50, 60, 70],
-    #    rec_text="Conv2D",
-    #    ch_w=5,
-    #    draw_skip=True,
-    #    skip_to_final=True)
     
     
     
-    x0, y0 = 300, 150
-    mydraw.canvas.create_text(x0, y0-30, text='resNetBlock', anchor='nw')
-    mydraw.canvas.create_oval(x0-5, y0-5, x0+5, y0+5, fill='black')
-    mydraw.canvas.create_text(x0+10, y0-10, text='x', anchor='nw')
-    mydraw.canvas.create_line(x0, y0+5, x0, y0+25, width=5)
-    coords_y = [y0, y0+23]
-    for i, s in enumerate([50, 50, 40]):
-        draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], s, s)
-        yf = coords_y[-1] + s + 20
-        coords_y.append(yf)
-    #draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], 20, 20, 
-    #    text='1x1', ch_w=1)
-    yf = coords_y[-1]
-    mydraw.canvas.create_oval(x0-5, yf-5, x0+5, yf+5, fill='black')
-    mydraw.canvas.create_text(x0+10, yf+5, text='x+delta', anchor='nw') 
-    mydraw.canvas.create_arc(x0-70, y0+10, x0+70, yf-10, 
-            extent=180, start=90, style=tk.ARC)
-  
-    x0, y0 = 450, 150
-    #mydraw.canvas.create_text(x0, y0-30, text='resNetBlock', anchor='nw')
-    mydraw.canvas.create_oval(x0-5, y0-5, x0+5, y0+5, fill='black')
-    mydraw.canvas.create_text(x0+10, y0-10, text='x', anchor='nw')
-    mydraw.canvas.create_line(x0, y0+5, x0, y0+25, width=5)
-    coords_y = [y0, y0+23]
-    for i, s in enumerate([50, 50]):
-        draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], s, s)
-        yf = coords_y[-1] + s + 20
-        coords_y.append(yf)
-    #draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], 20, 20, 
-    #    text='1x1', ch_w=1)
-    yf = coords_y[-1]
-    mydraw.canvas.create_oval(x0-5, yf-5, x0+5, yf+5, fill='black')
-    mydraw.canvas.create_text(x0+10, yf+5, text='x+delta', anchor='nw') 
-    mydraw.canvas.create_arc(x0-70, y0+10, x0+70, yf-10, 
-            extent=180, start=90, style=tk.ARC)    
-
-    x0, y0 = 600, 150
-    #mydraw.canvas.create_text(x0, y0-30, text='resNetBlock', anchor='nw')
-    mydraw.canvas.create_oval(x0-5, y0-5, x0+5, y0+5, fill='black')
-    mydraw.canvas.create_text(x0+10, y0-10, text='x', anchor='nw')
-    mydraw.canvas.create_line(x0, y0+5, x0, y0+25, width=5)
-    coords_y = [y0, y0+23]
-    for i, s in enumerate([50]):
-        draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], s, s)
-        yf = coords_y[-1] + s + 20
-        coords_y.append(yf)
-    #draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], 20, 20, 
-    #    text='1x1', ch_w=1)
-    yf = coords_y[-1]
-    mydraw.canvas.create_oval(x0-5, yf-5, x0+5, yf+5, fill='black')
-    mydraw.canvas.create_text(x0+10, yf+5, text='x+delta', anchor='nw') 
-    mydraw.canvas.create_arc(x0-70, y0+10, x0+70, yf-10, 
-            extent=180, start=90, style=tk.ARC)    
-
-
-    x0, y0 = 800, 100
-    mydraw.canvas.create_oval(x0-3, y0-3, x0+3, y0+3, fill='black')
-    mydraw.canvas.create_text(x0+10, y0-10, text='vt0 (input field)', anchor='nw')
-    mydraw.canvas.create_line(x0, y0+3, x0, y0+23, width=1)
-    coords_y = [y0, y0+23]
-    for i, s in enumerate([70, 70, 70]):
-        draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], s, s, 
-            text="resNet_"+str(i))
-        yf = coords_y[-1] + s + 20
-        coords_y.append(yf)
-    draw_conv2d_block(mydraw.canvas, x0, coords_y[-1], 20, 20, 
-        text='1x1', ch_w=1, draw_skip=False)
-    yf = coords_y[-1]
-    yf = yf + 40
-    mydraw.canvas.create_oval(x0-3, yf-3, x0+3, yf+3, fill='black')
-    mydraw.canvas.create_text(x0+10, yf+3, text='cnn output', anchor='nw')
-    mydraw.canvas.create_arc(x0-70, y0+10, x0+70, yf-10, 
-            extent=180, start=90, dash=(10,4), style=tk.ARC)
-    """
     mydraw.mainloop()
 
