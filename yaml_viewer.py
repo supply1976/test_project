673e9965903e9172807c5e6eef899973a8b0359d
Rich <tacowu@outlook.com>
2023-07-10 08:44:40 +0800

    code fix

--- yaml_viewer.py
+++ yaml_viewer.py
@@ -1,4 +1,5 @@
 import os, sys
+from functools import reduce
 import yaml
 import tkinter as tk
 import ttk
@@ -9,113 +10,131 @@ import ttk
 from tkinter import filedialog
 
 _GRID_CFG1 = {'padx':20, 'ipadx': 10, 'ipady':5, 'sticky':'W'}
+_GRID_CFG2 = {'padx':5, 'ipadx': 5, 'ipady':5, 'sticky':'W'}
 
 
 
-class TrainingUI(tk.Frame):
-    def __init__(self, master=None):
-        super(TrainingUI, self).__init__(master)
-        self.root_name = "TRAINING"
-        # system keys
-        self.key_gpu_num ="|SYSTEM|GPU_NUM"
-        self.key_gpu_pool="|SYSTEM|GPU_POOL"
-        
-        # flow keys
-        self.key_output_dir = "|FLOW|OUTPUT_DIR"
-        self.key_epo_report = "|FLOW|EPOCHS_TO_REPORT"
-        self.key_ckpt_export= "|FLOW|EXPORT_INTERMEDIATE_CKPT"
-        
-        # model export keys
-        self.key_mtp_path = "|AUTO_MODEL_EXPORT|MTP_PATH"
-        
-        # cnn keys
-        self.key_cnn_inputs = "|CNN|INPUT_IMAGE_NAMES"
-        self.key_cnn_init_algo= "CNN|INITIALIZATION|NETWORK_INITIALIZER"
+def open_new(root, ndict):
+    newWindow = tk.Toplevel(root)
+    appX = SectionUI(newWindow, "test")
+    appX.gen_view(ndict)
+    appX.pack()
 
 
-
-class SectionUI(tk.Frame):
-    def __init__(self, master, section_name, nestdict):
-        super(SectionUI, self).__init__(master)
+class SectionFrame(tk.Frame):
+    def __init__(self, master, sec_name):
+        super(SectionFrame, self).__init__(master, name=sec_name.lower())
+        self.master = master
+        self.sec_name = sec_name
+        self.levels = 0
         self.keys=[]
-        for i, (sub_k, sub_v) in enumerate(nestdict.items()):
-            if type(sub_v) is not dict:
-                self.keys.append("|".join([section_name, sub_k]))
-                tk.Label(app, text=sub_k).grid(row=i, column=0, **_GRID_CFG1)
-                tk.Label(app, text=sub_v).grid(row=i, column=1, **_GRID_CFG1)
+        self.btn_widgets=[]
+        self.ent_widgets=[]
+        
+    def show_next(self, parent, nestdict):
+        self.levels +=1
+        # 1st 
+        for i, (k, v) in enumerate(nestdict.items()):
+            if type(v) is not dict:
+                _key = "|".join([self.sec_name, k])
+                self.keys.append(_key)                
+                lb = tk.Label(parent, text=k)
+                en = tk.Entry(parent, name=k.lower())
+                if v is not None: en.insert(0, v)
+                #en.config(state='disabled')
+                if self.levels >=3:
+                    lb.grid(row=0, column=i, **_GRID_CFG1)
+                    en.grid(row=1, column=i, **_GRID_CFG1)
+                else:
+                    lb.grid(row=i, column=0, **_GRID_CFG1)
+                    en.grid(row=i, column=1, **_GRID_CFG1)
+
                 
             else:
-                sub2_dict = sub1_dict[sub1_k]
-                lb1 = tk.LabelFrame(app, text=sub1_k)
-                lb1.grid(row=i, column=0, columnspan=2, **_GRID_CFG1)
-                
-            
-        #self.lbfr1 = tk.LabelFrame(self, text="SECTION 1")
-        #self.lbfr1.pack(padx=20, pady=10, fill='both')
-        #tk.Label(self.lbfr1, text="foo").grid(
-        #    row=0, column=0)
-        #self.lbfr2 = tk.LabelFrame(self, text="SECTION 2")
-        #tk.Label(self.lbfr2, text="foo2").grid(
-        #    row=0, column=0)
-        #self.lbfr2.pack()
-
+                assert type(v) is dict
+                subdict = v
+                lbfr = tk.LabelFrame(parent, text=k, name=k.lower())
+                if k=='CNN':
+                    lbfr.grid(row=0, column=4, rowspan=4)
+                    tk.Button(lbfr, text="CNN SETTINGS").pack()
+                else:
+                    lbfr.grid(row=i, column=0, columnspan=4, **_GRID_CFG1)
+                    self.show_next(lbfr, subdict)
+                    
+                """
+                # 2nd
+                for j, (sk, sv) in enumerate(subdict.items()):
+                    _key = "|".join([self.sec_name, k, sk])
+                    self.keys.append(_key)
+                    
+                    if type(sv) is not dict:
+                        if sv is None: sv='none'
+                        tk.Label(lb, text=sk).grid(row=j, column=0, **_GRID_CFG1)
+                        en = tk.Entry(lb, name=sk.lower())
+                        en.insert(0, sv)
+                        #en.config(state='disabled')
+                        en.grid(row=j, column=1, **_GRID_CFG1)
+                    else:
+                        assert type(sv) is dict
+                        btn=tk.Button(lb, text=sk, name=sk.lower())
+                        self.btn_widgets.append(btn)
+                        if k=='CNN':
+                            btn.grid(row=j, column=0, columnspan=2, **_GRID_CFG1)
+                        else:
+                            btn.grid(row=0, column=2+j, rowspan=4, **_GRID_CFG2)
+                """
+        # end of for loop
+        return 0
         
+    def print_keys(self):
+        print(self.keys)
+                
+    def _foo(self):
+        pass
 
-class Tab2UI(tk.Frame):
-    def __init__(self, master=None):
-        super(Tab2UI, self).__init__(master)
-        self.lbfr1 = tk.LabelFrame(self, text="PlaceHolder 1")
-        self.lbfr1.pack()
-        self.lbfr2 = tk.LabelFrame(self, text="PlaceHolder 2")
-        self.lbfr2.pack()
-        self.pack()
+
+def get_update(root, yamldict, key):
+    yaml_val = reduce(lambda y, x: y[x], key.split("|"), yamldict)
+    print(yaml_val)
+    key = '!notebook.' + key.lower().replace("|", ".")
+    en_w = root.nametowidget(key)
+    print(en_w.get())
+    
 
 
 def main():
     root = tk.Tk()
     root.title("Yaml Viewer")
-    root.geometry('1400x1000')
+    root.geometry('1024x768')
     root.configure(bg='#aaffff')
     #root.option_add("*Font", "courier 12")
-    root.option_add("*Font", "NewTimes 10")
+    #root.option_add("*Font", "NewTimes 10")
     
     notebook = ttk.Notebook(root)
+    
     with open(sys.argv[1], 'r') as f:
         yamldict = yaml.full_load(f)
     
-    apps = []
-    for section_key in yamldict.keys():
-        app = tk.Frame(master=notebook)
-        app.pack()
-        
-        sub1_dict = yamldict[section_key]
-        for i, (sub1_k, sub1_v) in enumerate(sub1_dict.items()):
-            if type(sub1_v) is not dict:
-                tk.Label(app, text=sub1_k).grid(row=i, column=0, **_GRID_CFG1)
-                tk.Label(app, text=sub1_v).grid(row=i, column=1, **_GRID_CFG1)
-            else:
-                sub2_dict = sub1_dict[sub1_k]
-                lb1 = tk.LabelFrame(app, text=sub1_k)
-                lb1.grid(row=i, column=0, columnspan=2, **_GRID_CFG1)
-                
-                for j, (sub2_k, sub2_v) in enumerate(sub2_dict.items()):
-                    if type(sub2_v) is not dict:
-                        tk.Label(lb1, text=sub2_k).grid(row=j, column=0, **_GRID_CFG1)
-                        tk.Label(lb1, text=sub2_v).grid(row=j, column=1, **_GRID_CFG1)
-                    else:
-                        lb2 = tk.LabelFrame(lb1, text=sub2_k)
-                        lb2.grid(row=0, column=2+j, padx=5, rowspan=5, ipadx=5, sticky='N')
-                        sub3_dict=sub2_dict[sub2_k]
-                        for k, (sub3_k, sub3_v) in enumerate(sub3_dict.items()):
-                            if type(sub3_v) is not dict:
-                                tk.Label(lb2, text=sub3_k).grid(row=k, column=0, **_GRID_CFG1)
-                                tk.Label(lb2, text=sub3_v).grid(row=k, column=1, **_GRID_CFG1)
-                            else:
-                                tk.Button(lb2, text=sub3_k).grid(row=k, column=0, **_GRID_CFG1)
-        #app = SectionUI(master=notebook, keys=yamldict[k].keys())
-        notebook.add(app, text=section_key)
-
+    for tab_name in yamldict.keys():
+        app = SectionFrame(master=notebook, sec_name=tab_name)
+        app.show_next(parent=app, nestdict=yamldict[tab_name])
+        notebook.add(app, text=tab_name)
+        if len(app.btn_widgets)!=0:
+            for w in app.btn_widgets:
+                print(w, w.winfo_name())
+    
     notebook.pack()
+
+    cfg_fe_proc_type = "FEATURE_EXTRACTOR|PROCESS_TYPE"
+    get_update(root, yamldict, cfg_fe_proc_type)
+    tk.Button(root, text="update", 
+        command=lambda: get_update(root, yamldict, cfg_fe_proc_type)).pack()
+    
+    #btn_cnn_struct = root.nametowidget('!notebook.training.cnn.structure')
+    
+    #btn_cnn_struct.config(
+    #    command=lambda: open_new(root, yamldict['TRAINING']['CNN']['STRUCTURE']))
+
     
     root.mainloop()
 
